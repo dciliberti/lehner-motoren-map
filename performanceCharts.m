6cf00cec76b70ae4f7d2d48a87e3bd7afad0dad7
Danilo Ciliberti <dancili@gmail.com>
2019-08-23 11:36:59 +0200

    Data smoothed for the first two charts

--- performanceCharts.m
+++ performanceCharts.m
@@ -28,6 +28,8 @@ V10 = csvread('data\V50.csv');
 V11 = csvread('data\V55.csv');
 V12 = csvread('data\V60.csv');
 
+scale = 1000; % Scale factor over RPM for better interpolation
+
 % Current	Input power     RPM     Momentum	Output power	Efficiency
 % A         W               /min	Ncm         W	            %
 c = [V1(:,1); V2(:,1); V3(:,1); V4(:,1); V5(:,1); V6(:,1); V7(:,1); ...
@@ -35,7 +37,7 @@ c = [V1(:,1); V2(:,1); V3(:,1); V4(:,1); V5(:,1); V6(:,1); V7(:,1); ...
 e = [V1(:,2); V2(:,2); V3(:,2); V4(:,2); V5(:,2); V6(:,2); V7(:,2); ...
     V8(:,2); V9(:,2); V10(:,2); V11(:,2); V12(:,2)]; % Input (electric) power
 r = [V1(:,3); V2(:,3); V3(:,3); V4(:,3); V5(:,3); V6(:,3); V7(:,3); ...
-    V8(:,3); V9(:,3); V10(:,3); V11(:,3); V12(:,3)]; % RPM
+    V8(:,3); V9(:,3); V10(:,3); V11(:,3); V12(:,3)] ./ scale; % RPM (scaled)
 t = [V1(:,4); V2(:,4); V3(:,4); V4(:,4); V5(:,4); V6(:,4); V7(:,4); ...
     V8(:,4); V9(:,4); V10(:,4); V11(:,4); V12(:,4)]; % Momentum (torque)
 s = [V1(:,5); V2(:,5); V3(:,5); V4(:,5); V5(:,5); V6(:,5); V7(:,5); ...
@@ -49,7 +51,7 @@ limCur = [V1(end,1); V2(end,1); V3(end,1); V4(end,1); V5(end,1); V6(end,1); V7(e
 limInp = [V1(end,2); V2(end,2); V3(end,2); V4(end,2); V5(end,2); V6(end,2); V7(end,2); ...
     V8(end,2); V9(end,2); V10(end,2); V11(end,2); V12(end,2)];
 limRPM = [V1(end,3); V2(end,3); V3(end,3); V4(end,3); V5(end,3); V6(end,3); V7(end,3); ...
-    V8(end,3); V9(end,3); V10(end,3); V11(end,3); V12(end,3)];
+    V8(end,3); V9(end,3); V10(end,3); V11(end,3); V12(end,3)] ./ scale;
 limMom = [V1(end,4); V2(end,4); V3(end,4); V4(end,4); V5(end,4); V6(end,4); V7(end,4); ...
     V8(end,4); V9(end,4); V10(end,4); V11(end,4); V12(end,4)];
 limPow = [V1(end,5); V2(end,5); V3(end,5); V4(end,5); V5(end,5); V6(end,5); V7(end,5); ...
@@ -58,27 +60,29 @@ limEta = [V1(end,6); V2(end,6); V3(end,6); V4(end,6); V5(end,6); V6(end,6); V7(e
     V8(end,6); V9(end,6); V10(end,6); V11(end,6); V12(end,6)];
 
 %% Calculate CURRENT from expected shaft power and RPM (propeller data)
+[xa, ya] = meshgrid((0:100:11000)./scale, 0:0.1:15);  % x: RPM, y: Ampere
+za = griddata(r,c,s,xa,ya);
+
+% Interpolate data to caclculate current drawn at propeller operating points
 MXCL(:,4) = griddata(s,r,c,MXCL(:,2),MXCL(:,3));
 MXCR(:,4) = griddata(s,r,c,MXCR(:,2),MXCR(:,3));
-[xa, ya] = meshgrid(0:100:11000, 0:0.1:15);  % x: RPM, y: Ampere
-za = griddata(r,c,s,xa,ya);
 
 % Plot section
 figure
 hold on
 surf(xa,ya,za,'FaceColor','interp','LineStyle','none')
-plot3(V1(:,3),V1(:,1),V1(:,5),'k--')
-plot3(V2(:,3),V2(:,1),V2(:,5),'k--')
-plot3(V3(:,3),V3(:,1),V3(:,5),'k--')
-plot3(V4(:,3),V4(:,1),V4(:,5),'k--')
-plot3(V5(:,3),V5(:,1),V5(:,5),'k--')
-plot3(V6(:,3),V6(:,1),V6(:,5),'k--')
-plot3(V7(:,3),V7(:,1),V7(:,5),'k--')
-plot3(V8(:,3),V8(:,1),V8(:,5),'k--')
-plot3(V9(:,3),V9(:,1),V9(:,5),'k--')
-plot3(V10(:,3),V10(:,1),V10(:,5),'k--')
-plot3(V11(:,3),V11(:,1),V11(:,5),'k--')
-plot3(V12(:,3),V12(:,1),V12(:,5),'k--')
+plot3(V1(:,3)./scale,V1(:,1),V1(:,5),'k--')
+plot3(V2(:,3)./scale,V2(:,1),V2(:,5),'k--')
+plot3(V3(:,3)./scale,V3(:,1),V3(:,5),'k--')
+plot3(V4(:,3)./scale,V4(:,1),V4(:,5),'k--')
+plot3(V5(:,3)./scale,V5(:,1),V5(:,5),'k--')
+plot3(V6(:,3)./scale,V6(:,1),V6(:,5),'k--')
+plot3(V7(:,3)./scale,V7(:,1),V7(:,5),'k--')
+plot3(V8(:,3)./scale,V8(:,1),V8(:,5),'k--')
+plot3(V9(:,3)./scale,V9(:,1),V9(:,5),'k--')
+plot3(V10(:,3)./scale,V10(:,1),V10(:,5),'k--')
+plot3(V11(:,3)./scale,V11(:,1),V11(:,5),'k--')
+plot3(V12(:,3)./scale,V12(:,1),V12(:,5),'k--')
 plot3(limRPM,limCur,limPow,'k-','LineWidth',2)
 climb = plot3(MXCL(:,3),MXCL(:,4),MXCL(:,2)+20,'or-','LineWidth',1.5);
 cruise = plot3(MXCR(:,3),MXCR(:,4),MXCR(:,2)+20,'oy-','LineWidth',1.5);
@@ -112,22 +116,30 @@ for i = 1:size(MXCR,1)
         'HorizontalAlignment','right')
 end
 
+% Scaling the RPM thick labels
+rpmLabel = xticklabels;
+for i = 1:numel(rpmLabel)
+    rpmLabel{i} = str2double(rpmLabel{i}) * scale;
+    rpmLabel{i} = num2str(rpmLabel{i});
+end
+xticklabels(rpmLabel)
+
 % Contour plot
 figure
 hold on
 [C,H] = contourf(xa,ya,za,0:50:700);
-plot(V1(:,3),V1(:,1),'k--')
-plot(V2(:,3),V2(:,1),'k--')
-plot(V3(:,3),V3(:,1),'k--')
-plot(V4(:,3),V4(:,1),'k--')
-plot(V5(:,3),V5(:,1),'k--')
-plot(V6(:,3),V6(:,1),'k--')
-plot(V7(:,3),V7(:,1),'k--')
-plot(V8(:,3),V8(:,1),'k--')
-plot(V9(:,3),V9(:,1),'k--')
-plot(V10(:,3),V10(:,1),'k--')
-plot(V11(:,3),V11(:,1),'k--')
-plot(V12(:,3),V12(:,1),'k--')
+plot(V1(:,3)./scale,V1(:,1),'k--')
+plot(V2(:,3)./scale,V2(:,1),'k--')
+plot(V3(:,3)./scale,V3(:,1),'k--')
+plot(V4(:,3)./scale,V4(:,1),'k--')
+plot(V5(:,3)./scale,V5(:,1),'k--')
+plot(V6(:,3)./scale,V6(:,1),'k--')
+plot(V7(:,3)./scale,V7(:,1),'k--')
+plot(V8(:,3)./scale,V8(:,1),'k--')
+plot(V9(:,3)./scale,V9(:,1),'k--')
+plot(V10(:,3)./scale,V10(:,1),'k--')
+plot(V11(:,3)./scale,V11(:,1),'k--')
+plot(V12(:,3)./scale,V12(:,1),'k--')
 plot(limRPM,limCur,'k-','LineWidth',2)
 climb = plot(MXCL(:,3),MXCL(:,4),'or-','LineWidth',1.5);
 cruise = plot(MXCR(:,3),MXCR(:,4),'oy-','LineWidth',1.5);
@@ -161,6 +173,14 @@ for i = 1:size(MXCR,1)
         ['J = ',num2str(MXCR(i,1)), ' \rightarrow'], ...
         'HorizontalAlignment','right')
 end
+
+% Scaling the RPM thick labels
+rpmLabel = xticklabels;
+for i = 1:numel(rpmLabel)
+    rpmLabel{i} = str2double(rpmLabel{i}) * scale;
+    rpmLabel{i} = num2str(rpmLabel{i});
+end
+xticklabels(rpmLabel)
 
 %% Calculate TORQUE and motor EFFICIENCY from expected shaft power and RPM (propeller data)
 MXCL(:,5) = griddata(s,r,t,MXCL(:,2),MXCL(:,3));
